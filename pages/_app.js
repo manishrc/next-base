c8ef7cacbb5074b9116a30079eca81d1ce7287d7
Manish Chiniwalar <hi@manishrc.com>
2022-09-21 23:32:00 +0200

    add auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import Segment from "@/components/Segment";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Segment>
-      <Component {...pageProps} />
-    </Segment>
+    <SessionProvider session={session}>
+      <Segment>
+        <Component {...pageProps} />
+      </Segment>
+    </SessionProvider>
   );
 }
 
