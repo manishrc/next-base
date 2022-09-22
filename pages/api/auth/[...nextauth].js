50bf4fc1f9285107db9656fbef827aaca13673c2
Manish Chiniwalar <hi@manishrc.com>
2022-09-22 10:43:53 +0200

    add db: planetscale

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import PrismaClient from "@/lib/prisma";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
