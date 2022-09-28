d082aba3b13e611202f0ad267c8477dc6cc79459
Manish Chiniwalar <hi@manishrc.com>
2022-09-28 22:30:19 +0200

    fix: import prisma client correctly

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/github";
 import FacebookProvider from "next-auth/providers/facebook";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import PrismaClient from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
